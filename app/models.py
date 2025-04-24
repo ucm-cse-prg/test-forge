8287b9e2e388e7dfd34eeac64f2d3fb1ee7680fa
Spencer Stafford <152045915+sstafford4@users.noreply.github.com>
2025-04-23 21:32:58 -0700

    Initial Commit

--- app/models.py
+++ app/models.py
@@ -4,3 +4,18 @@ Data Models based on Pydantic Models
 These Models form the backbone of the application, providing a way to validate and serialize data.
 Request/response schemas as well as database documents are derived from these models.
 """
+from pydantic import BaseModel, Field
+
+
+class UploadResponse(BaseModel):
+    filename: str = Field(default="")
+    s3_key: str
+    url: str 
+
+class GetFile(BaseModel):
+    filename: str = Field(default="")
+    s3_key: str = Field(default="")
+    url: str
+
+class UpdateRequest(BaseModel):
+    new_filename: str = Field(default="")
