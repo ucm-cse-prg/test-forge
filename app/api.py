092a66a71ee59309f83ea585f618ea6e0b31eb60
Spencer Stafford <152045915+sstafford4@users.noreply.github.com>
2025-04-23 21:30:21 -0700

    Initial Commit

--- app/api.py
+++ app/api.py
@@ -1,15 +1,15 @@
-"""
-API endpoints for the application.
-"""
-
 import typing
+from typing import List, Optional
 from functools import wraps
+from botocore.exceptions import ClientError
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, UploadFile, File
 from fastapi.responses import HTMLResponse
 
-from app.actions import home_page
+import app.actions as Actions
 from app.exceptions import APIException
+from app.models import UploadResponse, GetFile, UpdateRequest
+
 
 router = APIRouter()
 
@@ -32,8 +32,64 @@ def http_exception(func):
 @router.get("/")
 @http_exception
 async def root() -> HTMLResponse:
-    content: str = await home_page()
+    content: str = await Actions.home_page()
     return HTMLResponse(
         content=content,
         status_code=200,
     )
+
+
+@router.post("/upload/")
+async def upload_material(file: UploadFile = File(None), uploader_id: Optional[str] = None) -> UploadResponse:
+    if file is None:
+        raise HTTPException(status_code=400, detail="File is required.")
+    try:
+        return await Actions.upload_file(file, uploader_id)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal Server Error{str(e)}")
+
+
+@router.delete("/delete/{s3_key}")
+async def delete_material(s3_key: str) -> None:
+    try:
+        return await Actions.delete_file(s3_key)
+    
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "NoSuchKey":
+            raise HTTPException(status_code=500, detail=f"No file found with key '{s3_key}'")
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get("/get_all/", response_model=List[GetFile])
+async def retrieve_all_files() -> List[GetFile]:
+    try:
+        return await Actions.get_all_files()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+@router.patch("/update/{s3_key}")
+async def update_material(s3_key: str, payload: UpdateRequest) -> dict:
+    try:
+        await Actions.update_file_metadata(s3_key, payload.new_filename)
+        return {"detail": "File metadata updated successfully."}
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    
+@router.patch("/replace/{s3_key}")
+async def replace_material(s3_key: str, file: UploadFile = File(None)) -> dict:
+    if file is None:
+        raise HTTPException(status_code=400, detail="File is required.")
+    try:
+        await Actions.replace_file(s3_key, file)
+        return {"detail": "File replaced successfully."}
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
